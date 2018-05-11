f40c2262ee6e1fb0e167588381376e844ddc33d2
Tales Ribeiro <tales.magalhaes@computacao.ufla.br>
2018-05-11 09:26:05 -0300

    Adicionei as mudanças do Marcos ao programa

--- trabalho.cpp
+++ trabalho.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 
 /* ENTRADAS
-
 Personagem de desenho animado
 ● Nome
 ● Criador
@@ -14,12 +13,12 @@ Personagem de desenho animado
 
 struct Desenho
 {
-    string Nome[50]; //Primeiro campo do registro. Utilizado para busca de um certo dado no caso o registro de um personagem de um desenho. Para efeitos de busca deve-se utilizar o Nome do personagem
-    string Criador[50];
-    string Titulo[50];
-    string Relacoes[50];
+    string Nome; //Primeiro campo do registro. Utilizado para busca de um certo dado no caso o registro de um personagem de um desenho. Para efeitos de busca deve-se utilizar o Nome do personagem
+    string Criador;
+    string Titulo;
+    string Relacoes;
 };
-Desenho personagem;
+Desenho personagem[50];	// como eu acho que cada personagem terá apenas um nome,criador,etc eu tirei os vetores das strings e coloquei na struct dos personagens
 
 int main(){
     cout << setw(53) << "PERSONAGENS DE DESENHO ANIMADO" << "\n\n";
@@ -28,25 +27,35 @@ int main(){
     int menu;
     cin >> menu;
     //bool excluir = false;
-    const int tam = 50;
+
+	int tam = 0; 	// variável do indice personagem a ser preenchido
+	while (menu != 5){		//Enquanto o usuário não comanda pra sair vai realizando as tarefas pedidas e no final é pedido outro menu.
+
     switch (menu)
     {
     case 1:
         cout << "Incluir: ";
         cout << "\n Qual e' o nome do personagem do desenho animado? ";
-        cin >> personagem.Nome[tam];
+        cin >> personagem[tam].Nome;
         cout << "\n Qual e' o nome do criador desse desenho? ";
-        cin >> personagem.Criador[tam];
+        cin >> personagem[tam].Criador;
         cout << "\n Qual é o titulo do desenho? ";
-        cin >> personagem.Titulo[tam];
+        cin >> personagem[tam].Titulo;
         cout << "\n Quais sao as principais relacoes desse desenho? ";
-        cin >> personagem.Relacoes[tam];
+        cin >> personagem[tam].Relacoes;
+        tam++;
         break;
     case 2:
         cout << "Excluir: ";
         break;
     case 3:
-        cout << "Listar: ";
+       { cout << "Listar: ";
+        int indice = 0;
+        while (indice < tam){
+			cout << "Nome do personagem: " << personagem[indice].Nome << endl << "Seu criador: " << personagem[indice].Criador << endl
+			<< "O titulo do desenho: " << personagem[indice].Titulo << endl << "Suas relacoes: " << personagem[indice].Relacoes << endl;
+			indice++;
+		}	}
         break;
     case 4:
         cout << "Alterar: ";
@@ -58,6 +67,7 @@ int main(){
         cout << "Comando invalido!";
         break;
     }
-
+    cin >> menu;
+  }
     return 0;
 }
